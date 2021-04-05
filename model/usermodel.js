cb18b113a7c345dea1ae61dd06ef7df47711337f
arshifty <arshifty@gmail.com>
2021-04-05 20:41:37 +0600

    first commit

---
+++ model/usermodel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema({
+  userName: {
+    type: String
+  },
+  contact: {
+    type: Number
+  },
+  password: {
+    type: String
+  },
+  city: {
+    type: String
+  },
+  country: {
+    type: String
+  },
+  code: {
+    type: String
+  },
+  bool: {
+    type: Boolean , default: false
+  }
+  
+  });
+  
+  module.exports = mongoose.model('userreg', PostSchema  );
