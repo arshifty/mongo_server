5810b54bd79c0dc26a77e408e9937baac12e427b
arshifty <arshifty@gmail.com>
2021-07-07 17:42:13 +0600

    ssd

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(cors());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -18,21 +18,36 @@ app.use(bodyParser.json());
 
 //import routes
 const userRoute = require('./router/userroute');
-app.use('/user', userRoute );
+app.use('/user', userRoute);
 
 const studentRoute = require('./router/studentroute');
-app.use('/student', studentRoute );
+app.use('/student', studentRoute);
 
 
 
-app.get('/',(req, res) =>{
+app.get('/', (req, res) => {
   console.log('test sss');
-   res.send('home test');
+  res.send('home test');
 });
 
-mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser:true } , () =>
- console.log('connected to database test ')
- );
+
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect('mongodb://127.0.0.1:27017/test', {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       ssl: true,
+//       sslValidate: true,
+//       sslCA: require('fs').readFileSync(`/etc/ssl/mongo_new/cert.pem`)
+//     });
+//   } catch (err) {
+//     console.log('Failed to connect to MongoDB', err);
+//   }
+// };
+
+// connectDB();
+
 
 
 
